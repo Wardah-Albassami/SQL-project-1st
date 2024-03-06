d87f2b81d9d2e3881aa3df44fd93a77a7cec7da8
wardward1818@gmail.com <wardward1818@gmail.com>
2024-03-06 06:27:03 +0000

    solved

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,8 +6,7 @@ SELECT
     quantity, 
     CAST(purchase_price AS FLOAT64), 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue-ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
-
