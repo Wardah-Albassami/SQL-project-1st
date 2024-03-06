7ec4e94e74fca00ba276d57ec44d0fe5b468cfd2
wardward1818@gmail.com <wardward1818@gmail.com>
2024-03-06 16:27:40 +0000

    edit

--- models/mart/finance_campaigns_month.sql
+++ models/mart/finance_campaigns_month.sql
@@ -1,23 +1,8 @@
-WITH monthly_data AS
-(
 SELECT
-*,
-extract(month from date_date) AS month
-FROM {{ ref('finance_campaigns_day') }}
-)
-SELECT 
-month
-    ,SUM(nb_transactions) AS monthly_nb_transactions
-    ,ROUND(SUM(revenue),0) AS revenue 
-    ,ROUND(AVG(average_basket),1) AS monthly_average_basket
-    ,ROUND(SUM(revenue)/SUM(nb_transactions),1) AS monthly_average_basket_bis
-    ,ROUND(SUM(margin),0) AS margin 
-    ,ROUND(SUM(operational_margin),0) AS operational_margin 
-    ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
-    ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
-    ,ROUND(SUM(logcost),0) AS log_cost 
-    ,ROUND(SUM(ship_cost),0) AS ship_cost 
-    ,SUM(quantity) AS quantity 
-FROM monthly_data
-GROUP BY month
-ORDER BY month
+    date_date,
+    SUM(ads_cost) as ads_cost,
+    SUM(impression) as ads_impression,
+    SUM(click) as ads_clicks
+FROM {{ ref("int_campaigns") }}
+GROUP BY date_date
+ORDER BY date_date DESC
